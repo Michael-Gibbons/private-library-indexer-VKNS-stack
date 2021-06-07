979b48ce555e47a3e184c95f7894a11bb8f5bbbc
Michael Gibbons <michael@btownweb.com>
2021-06-06 18:41:13 -0700

    basic prevent duplicate books from isbn

--- server/routes/api/v1/db.js
+++ server/routes/api/v1/db.js
@@ -7,19 +7,22 @@ const BASE_URL = '/api/v1/db';
 //get records
 router.get(BASE_URL, ctx => ctx.body ='Hello from the db route');
 //add record
-router.get(BASE_URL + '/create', ctx => {
-  Books.create({
+router.get(BASE_URL + '/create', async ctx => {
+  const BookToAdd = {
     title: "testTitle",
-    isbn: "3",
+    isbn: "4",
     genre: "Fantasy",
     synopsis: "Is a pretty good book",
     numberOfPages: "420",
     seriesName: "Series name",
     authorName: "Michael Gibbons",
     authorBio: "Devilishly Handsome"
+  }
+
+  const [book, created] = await Books.findOrCreate({
+    where: { isbn: BookToAdd.isbn },
+    defaults: BookToAdd
   });
-
-
 });
 //delete record
 
