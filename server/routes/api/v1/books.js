12f686255b90795284159fcdcc6d751ce3c75953
Michael Gibbons <michael@btownweb.com>
2021-06-13 16:28:55 -0700

    Changed put to patch since put is not needed

--- server/routes/api/v1/books.js
+++ server/routes/api/v1/books.js
@@ -62,15 +62,17 @@ router.post('/books', async ctx => {
       ctx.response.body = book;
       return;
     }
+
     ctx.status = 409;
     ctx.response.body = {error: {message: 'duplicate ISBN found.' }};
+    
   }catch(err){
     ctx.app.emit('error', err, ctx);
   }
 });
 
-//replace a book's data PUT /books/:id
-router.put('/books/:isbn', async ctx => {
+//replace a book's data PATCH /books/:id
+router.patch('/books/:isbn', async ctx => {
   try{
     const ALLOWED_QUERY_PARAMETERS = getAllowedQueryParameters(Books);
     const updates = queryFormat(getDefinedSubset(ALLOWED_QUERY_PARAMETERS, ctx.request.query));
@@ -84,7 +86,12 @@ router.put('/books/:isbn', async ctx => {
       const book = await Books.findOne({where: { isbn: ctx.params.isbn }});
       ctx.status = 200;
       ctx.response.body = book;
+      return
     }
+
+    ctx.status = 404;
+    ctx.response.body = {error: {message: `No book in database with ISBN: ${ctx.params.isbn}` }};
+
   }catch(err){
     ctx.app.emit('error', err, ctx);
   }
@@ -99,8 +106,10 @@ router.delete('/books/:isbn', async ctx =>{
       ctx.response.body = {message: 'Book Deleted Successfully'};
       return
     }
+
     ctx.status = 404;
     ctx.response.body = {error: {message: `No book in database with ISBN: ${ctx.params.isbn}` }};
+
   }catch(err){
     ctx.app.emit('error', err, ctx);
   }
